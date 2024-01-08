5753182055b19d63d5c5880a80ddce9c8409b9b2
Eemeli Koikkalainen <eemeli.koikkalainen@outlook.com>
2024-01-08 02:42:26 +0200

    Fixed telescope opening files in insert mode

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- This somehow fixes telescope opening files in insert mode (https://github.com/nvim-telescope/telescope.nvim/issues/2027)
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
